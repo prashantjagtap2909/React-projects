296fb3c0d12fad2905d6998094ebd23b688c8e5a
Prashant Jagtap <prashantjagtap06532@gmail.com>
2024-03-16 18:35:34 +0530

    added props , components example

---
+++ ReactFolio/demoshopapp/src/components/ItemDate.js
@@ -0,0 +1,24 @@
+import './ItemDate.css';
+
+function ItemDate(props){
+
+    const day = props.day;
+    const month = props.month;
+    const year = props.year;
+
+return ( 
+
+    <div className='date'>
+
+        <span> {day}</span>
+        <span> {month} </span>
+        <span> {year} </span>
+
+
+    </div>
+);
+
+
+}
+
+export default ItemDate;
