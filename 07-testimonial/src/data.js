789eccaa843ff82b0a580b630031bc1eb26852ec
Prashant Jagtap <prashantjagtap06532@gmail.com>
2024-03-18 20:57:32 +0530

    added project top courses and testimonials

---
+++ 07-testimonial/src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Arjun Kadam",
+    job: "Software Engineer",
+    image: "https://avatars.githubusercontent.com/u/103011996?v=4",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Shubham Bhoite",
+    job: "Graphic Designer",
+    image: "https://avatars.githubusercontent.com/u/117765637?v=4",
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Sham Machale",
+    job: "Marketing Manager",
+    image: "https://avatars.githubusercontent.com/u/120729282?v=4",
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Mahesh Nikam",
+    job: "Content Strategist",
+    image: "https://avatars.githubusercontent.com/u/123966043?v=4",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Vaibhav Gaikwad",
+    job: "Data Analyst",
+    image: "https://avatars.githubusercontent.com/u/124031895?v=4",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
